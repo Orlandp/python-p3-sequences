53a83a29b00a0a3f58bbd7863f1583d00a534fdd
pknowledge <jamesblssings@gmail.com>
2025-08-10 08:44:12 +0300

    sequences in python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    if length == 1:
+        print([0])
+        return
+
+    seq = [0, 1]
+    for i in range(2, length):
+        seq.append(seq[i - 1] + seq[i - 2])
+
+    # Print the final sequence once
+    print(seq)
